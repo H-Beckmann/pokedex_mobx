e5d3ea2da91d8ebaedde160a4bde7da2e07f0161
Beckmann <henriquebeckmann10@gmail.com>
2021-11-01 21:21:04 -0300

    :sparkles: Listagem de pokemons completa

--- lib/UI/pokemon_listining.dart
+++ lib/UI/pokemon_listining.dart
@@ -1,10 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:pokedex_mobx/models/pokemon.dart';
+import 'package:pokedex_mobx/models/type_color.dart';
 import 'package:pokedex_mobx/repositories/pokemon_repository.dart';
 import 'package:transparent_image/transparent_image.dart';
 
 class PokemonList extends StatefulWidget {
-  const PokemonList({ Key? key, required this.title }) : super(key: key);
+  const PokemonList({Key? key, required this.title}) : super(key: key);
   final String title;
 
   @override
@@ -16,56 +17,93 @@ class _PokemonListState extends State<PokemonList> {
   @override
   void initState() {
     super.initState();
-    pokeList = fetchAll();
+    pokeList = fetchAll(50);
   }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text(widget.title),
-        leading: const Icon(Icons.catching_pokemon)
-      ),
-      backgroundColor: Colors.grey[200],
-      body: FutureBuilder<List<Pokemon>>(
-        future: pokeList,
-        builder: (context, snapshot) {
-          return GridView.builder(
-          padding: const EdgeInsets.all(10.0),
-          gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-            crossAxisCount: 2,
-            crossAxisSpacing: 10.0,
-            mainAxisSpacing: 10.0,
-          ), 
-          itemCount: 20,
-          itemBuilder: (context, index){
-            if(index < 20) {
-              return GestureDetector(
-                child: FadeInImage.memoryNetwork(
-                  placeholder: kTransparentImage, 
-                  image: snapshot.data!.elementAt(index).image,
-                  height: 300.0,
-                  fit: BoxFit.cover,
-                ),
-                onTap: (){ },
-              );
-            } else {
-              return GestureDetector(
-                child: Column(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: const <Widget>[
-                    Icon(Icons.add, color: Colors.white, size: 70.0,),
-                    Text(
-                      "Carregar mais...",
-                      style: TextStyle(color: Colors.white, fontSize: 22.0),
-                    )
-                  ],
-                ),
-              );
+        appBar: AppBar(
+            title: Text(widget.title),
+            leading: const Icon(Icons.catching_pokemon)),
+        backgroundColor: Colors.grey[200],
+        body: FutureBuilder<List<Pokemon>>(
+          future: pokeList,
+          builder: (context, snapshot) {
+            if (snapshot.hasData) {
+              return GridView.builder(
+                  padding: const EdgeInsets.all(10.0),
+                  gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+                    crossAxisCount: 2,
+                    crossAxisSpacing: 10.0,
+                    mainAxisSpacing: 10.0,
+                  ),
+                  itemCount: 50,
+                  itemBuilder: (context, index) {
+                    if (index < 50) {
+                      return DecoratedBox(
+                        decoration: BoxDecoration(
+                          color: fromHex(getColor(snapshot.data![index].types[0].name)),
+                          borderRadius: BorderRadius.circular(15),
+                          border: const Border(
+                            bottom: BorderSide(
+                              color: Colors.black,
+                              width: 3
+                            ),
+                            left: BorderSide(
+                              color: Colors.black,
+                              width: 3
+                            ),
+                            top: BorderSide(
+                              color: Colors.black,
+                              width: 3
+                            ),
+                            right: BorderSide(
+                              color: Colors.black,
+                              width: 3
+                            ),
+                          )
+                        ),
+                        child: GestureDetector(
+                          child: Column(
+                            children: [
+                              FadeInImage.memoryNetwork(
+                                placeholder: kTransparentImage,
+                                image: snapshot.data![index].image,
+                                height: 150.0,
+                                fit: BoxFit.cover,
+                              ),
+                              Text(snapshot.data![index].name)
+                            ],
+                          ),
+                          onTap: () {},
+                        ),
+                      );
+                    } else {
+                      return GestureDetector(
+                        child: Column(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: const <Widget>[
+                            Icon(
+                              Icons.add,
+                              color: Colors.white,
+                              size: 70.0,
+                            ),
+                            Text(
+                              "Carregar mais...",
+                              style: TextStyle(
+                                  color: Colors.white, fontSize: 22.0),
+                            )
+                          ],
+                        ),
+                      );
+                    }
+                  });
+            } else if (snapshot.hasError) {
+              return Text('{$snapshot.error}');
             }
-          }
-    );
-        }
-      )
-    );
+            return const Center(child: CircularProgressIndicator());
+          },
+        ));
   }
 }
--- lib/repositories/pokemon_repository.dart
+++ lib/repositories/pokemon_repository.dart
@@ -12,14 +12,16 @@ Future<Pokemon> fetchPokemon(int id) async {
   }
 }
 
-Future<List<Pokemon>> fetchAll() async{
-  final http.Response response =
-      await http.get(Uri.parse('https://pokeapi.co/api/v2/pokemon?limit20'));
-  if (response.statusCode == 200) {
-    var r = json.decode(response.body);
-    List<Pokemon> list = List<Pokemon>.from(r['results'].map((Pokemon)=> Pokemon.fromJson(Pokemon)));
-    return list;
-  } else {
-    throw Exception('Failed to load a Pokemon');
+Future<List<Pokemon>> fetchAll(int limit) async{
+  List<Pokemon> list= List.empty(growable: true);
+  for (var id = 1; id <= limit; id++) {
+    final http.Response response = 
+        await http.get(Uri.parse('https://pokeapi.co/api/v2/pokemon-form/$id/'));
+    if(response.statusCode==200) {
+      list.add(Pokemon.fromJson(jsonDecode(response.body)));
+    } else{
+      throw Exception("Failed to catch the pokemon");
+    }
   }
+  return list;
 }
